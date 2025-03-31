26d1de28a2878421fc74085e78809f93647e364b
darko2019 <darko.ilic83@gmail.com>
2025-04-01 01:37:48 +0200

    Promenjen email na kontakt stranici

--- kontakt.php
+++ kontakt.php
@@ -6,7 +6,7 @@ if (isset($_POST["submit"])) {
     $message = $_POST['message'];
 
 
-    $to = 'office@xotravel.net';
+    $to = 'xotravelkombiprevoz@gmail.com';
     $subject = 'Contact form';
     $body = "Ime: $name\r\n\nE-mail: $email\n\nTelefon: $telefon\n\nPoruka: $message";
 
